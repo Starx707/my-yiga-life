e841607aa06185a33b0c188347dbd0bc6d4c54cb
Star <101126792+Starx707@users.noreply.github.com>
2025-10-15 11:02:03 +0200

    Added data for table and will make another table for foreign connection

--- database/migrations/2025_10_15_073936_create_posts_table.php
+++ database/migrations/2025_10_15_073936_create_posts_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,13 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+//            $table->foreignId('category_id')->constrained();
+            $table->integer('yiga_points');
+            $table->string('details', 300);
+            $table->integer('likes');
+            $table->string('img');
+            $table->string('location');
+            $table->boolean('private');
             $table->timestamps();
         });
     }
